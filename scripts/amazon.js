a40e4dd0f2527e17df7061af61e8160ea72498db
theMungai <mungaithesimon@gmail.com>
2024-11-16 12:00:49 +0300

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,6 +77,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
             });
         }
 
-        console.log(cart)
+        let cartQuantity = 0
+        cart.forEach((item) => {
+            cartQuantity = cartQuantity + item.quantity
+        });
+
+        document.querySelector(".js-cart-quantity").innerHTML = cartQuantity
     });
 });
