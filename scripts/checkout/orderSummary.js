951abf8bdddf5b0c8ffb3441e158e49db188fba9
theMungai <mungaithesimon@gmail.com>
2024-11-18 17:12:42 +0300

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,14 +4,9 @@ import {formatCurrency } from '../utils/money.js';
 import {hello} from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 
-hello()
-
-const today = dayjs()
-const deliveryDate = today.add(7, 'days');
-
-console.log(deliveryDate.format('dddd, MMMM D YYYY'))
 
 export function renderOrderSummary(){
 
@@ -114,7 +109,7 @@ export function renderOrderSummary(){
         link.addEventListener('click', () => {
             const productId = link.dataset.productId
             removeFromCart(productId);
-            
+            renderPaymentSummary()
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove()
         });
@@ -126,6 +121,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary()
         });
     });
 }
