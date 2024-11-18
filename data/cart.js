c6d6f1fc6bf3ed58b05e3f96da8567a820d1dbbd
theMungai <mungaithesimon@gmail.com>
2024-11-18 08:58:55 +0300

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [] /* This variable can be used outside of cart.js without causing naming conflicts since we are exporting it. */
+
+
+export function addToCart(productId){
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    })
+
+    if(matchingItem){
+        matchingItem.quantity += 1
+    }
+    else {
+        cart.push({
+            productId : productId,
+            quantity : 1
+        });
+    }
+}
