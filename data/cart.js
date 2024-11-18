909dfcff960a7db9686c39ead8223ac80ccba786
theMungai <mungaithesimon@gmail.com>
2024-11-18 09:30:18 +0300

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [] /* This variable can be used outside of cart.js without causing naming conflicts since we are exporting it. */
+export const cart = [
+    {
+        productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity : 2
+    },
+    {
+        productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity : 1
+    }
+] /* This variable can be used outside of cart.js without causing naming conflicts since we are exporting it. */
 
 
 export function addToCart(productId){
