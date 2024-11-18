f1e7bdb9e77ed45ae915e0ee5bd3ef0e4ba82b0a
theMungai <mungaithesimon@gmail.com>
2024-11-18 10:13:32 +0300

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,26 @@
-export let cart = [
-    {
-        productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity : 2
-    },
-    {
-        productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity : 1
-    }
-] /* This variable can be used outside of cart.js without causing naming conflicts since we are exporting it. */
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if(!cart){
+    cart = [
+        {
+            productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity : 2
+        },
+        {
+            productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity : 1
+        }
+    ] /* This variable can be used outside of cart.js without causing naming conflicts since we are exporting it. */
+    
+}
+
+
+
+// using localStorage to save our cart
+
+function saveToStorage () {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
@@ -27,6 +39,8 @@ export function addToCart(productId){
             quantity : 1
         });
     }
+
+    saveToStorage ()
 }
 
 export function removeFromCart(productId){
@@ -39,5 +53,7 @@ export function removeFromCart(productId){
         }
     });
 
-    cart = newCart
+    cart = newCart;
+
+    saveToStorage ()
 }
