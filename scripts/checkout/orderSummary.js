aad4d2052bce8504f099005298b0f34f63ee9516
theMungai <mungaithesimon@gmail.com>
2024-11-19 12:11:53 +0300

    Create oop version of cart

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -28,7 +28,7 @@ export function renderOrderSummary(){
         );
 
         cartSummaryHTML += `
-        <div class="cart-item-container 
+        <div class="cart-item-container js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: ${dateString}
@@ -45,7 +45,7 @@ export function renderOrderSummary(){
                 <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
